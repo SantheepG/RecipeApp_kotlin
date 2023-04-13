769adb56f8954976b0172cdcef20f1dae65f620f
[Santheep Ganeswaran] <[gshantheep@gmail.com]>
2023-04-13 07:38:51 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mobilecw2/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.mobilecw2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+//Cut out q: where will be the database stored locally
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/mobilecw2/SearchMeal.kt
@@ -0,0 +1,4 @@
+package com.example.mobilecw2
+
+class SearchMeal {
+}
